d2d9d1b2df744810e2ba610e4725f8f243df596c
Lindsay Carr <lcarr@usgs.gov>
2018-02-14 11:59:01 -0600

    format + correct filename

--- scripts/execute_preprocessing.R
+++ scripts/execute_preprocessing.R
@@ -1,8 +1,10 @@
 ## commands to run the preprocessing steps
 
 # run the full preprocesing workflow
-remake::make(remake_file = "preprocessing.yaml")
+# remake::make(remake_file = "preprocessing.yaml")
+
+system.time(remake::make(remake_file = "preprocess.yaml"))
 
 # run an individual target:
 remake::make(target_names = "county_boundaries_topojson", 
-             remake_file = "preprocessing.yaml")
+             remake_file = "preprocess.yaml")
