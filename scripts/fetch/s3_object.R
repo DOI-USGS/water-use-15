f8cb41e02241bc92c635e28d309c8cd0dcff6fb6
Lindsay Carr <lcarr@usgs.gov>
2018-02-14 11:59:20 -0600

    save instead of get for s3 download

--- scripts/fetch/s3_object.R
+++ scripts/fetch/s3_object.R
@@ -4,8 +4,8 @@ fetch.s3_object <- function(viz){
   aws.signature::use_credentials(profile='default', file=aws.signature::default_credentials_file())
   
   # download object from an s3 bucket
-  object_fn <- aws.s3::get_object(object = deps[["object_name"]], 
-                                  bucket = deps[["bucket_name"]],
-                                  file = viz[["location"]])
+  object_fn <- aws.s3::save_object(object = deps[["object_name"]], 
+                                   bucket = deps[["bucket_name"]],
+                                   file = viz[["location"]])
   return(object_fn)
 }
