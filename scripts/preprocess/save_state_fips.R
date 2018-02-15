eaf9d161a3c6254ab12f88dcc1911a01c9efe23b
Lindsay Carr <lcarr@usgs.gov>
2018-02-15 16:06:25 -0600

    add r package to function call

--- scripts/preprocess/save_state_fips.R
+++ scripts/preprocess/save_state_fips.R
@@ -5,7 +5,7 @@ save_state_fips <- function(location, zipfilepath, jsonfilepath) {
   unzip(zipfilepath, files = jsonfilepath, exdir = dir_name)
   
   # read json and create vector of just fips
-  states_info <- fromJSON(file.path(dir_name, jsonfilepath))
+  states_info <- jsonlite::fromJSON(file.path(dir_name, jsonfilepath))
   fips <- states_info[["state_FIPS"]]
   fips <- c("01", "04") # limit to AZ and AL for now
   
