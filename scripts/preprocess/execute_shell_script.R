3fb8e7969db279a342922f384751155dc68efebc
Lindsay Carr <lcarr@usgs.gov>
2018-02-14 18:18:44 -0600

    loop & pass args into shell script

--- scripts/preprocess/execute_shell_script.R
+++ scripts/preprocess/execute_shell_script.R
@@ -1,6 +1,10 @@
-execute_shell_script <- function(location, shell_script_fn){
+execute_shell_script <- function(location, zipfilepath, shell_script_fn){
   
-  cmd <- paste("bash", shell_script_fn)
+  ## can only work with one fips code right now
+  ## need to figure out how to make an array pass in as one arg
+  statefips <- paste(c("01"), collapse = " ") 
+  cmd <- paste("bash", shell_script_fn, zipfilepath, statefips, location)
+  
   system(cmd)
   
 }
--- scripts/preprocess/topo_county_boundaries.sh
+++ scripts/preprocess/topo_county_boundaries.sh
@@ -4,38 +4,42 @@
 TMP=$(mktemp -d)
 
 # unzip the geojson
-unzip cache/county_boundaries_geojson.zip -d $TMP
+unzip $1 -d $TMP
 
 # pick out the geojson files (exclude counties.js and states.js)
 GJ=$(dir $TMP/*.geojson)
 
+# list state fips for now
+statefips=$2
+
+for fip in $statefips
+
+do
+
+echo $fip
+
 # convert to topojson
 geo2topo \
-  state=$TMP/01.geojson \
-  -o $TMP/01.json
-geo2topo \
-  state=$TMP/04.geojson \
-  -o $TMP/04.json
+  state=$TMP/$fip.geojson \
+  -o $TMP/$fip.json
 
 # simplify
 toposimplify -s 1e-4 -f \
-  $TMP/01.json \
-  -o $TMP/01-simple.json
-toposimplify -s 1e-4 -f \
-  $TMP/04.json \
-  -o $TMP/04-simple.json
+  $TMP/$fip.json \
+  -o $TMP/$fip-simple.json
 
 # quantize (store as integers, scale later)
 topoquantize 1e5 \
-  $TMP/01-simple.json \
-  -o $TMP/01-quantized.json
-topoquantize 1e5 \
-  $TMP/04-simple.json \
-  -o $TMP/04-quantized.json
+  $TMP/$fip-simple.json \
+  -o $TMP/$fip-quantized.json
+  
+done
+
+echo All done
 
 # zip back up for storage in cache/
 WD=$(pwd)
 cd "$TMP"
-zip "$WD/cache/county_boundaries_topojson.zip" ./*quantized.json states.json counties.json
+zip "$WD/$3" ./*quantized.json states.json counties.json
 cd "$WD"
 
