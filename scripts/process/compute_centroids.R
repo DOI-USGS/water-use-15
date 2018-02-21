93f0f7c2763335e3179f204e42372e62d063a7f1
Alison Appling <aappling@usgs.gov>
2018-02-21 16:46:02 -0700

    compute centroids, save as topojson

---
+++ scripts/process/compute_centroids.R
@@ -0,0 +1,32 @@
+process.compute_centroids <- function(viz) {
+  zipfile <- readDepends(viz)$zipfile
+  
+  # unzip to a temp dir
+  exdir <- file.path(tempdir(), 'census')
+  if(!dir.exists(exdir)) dir.create(exdir)
+  unzip(zipfile, exdir=exdir)
+  shpfiles <- normalizePath(dir(exdir, full.names=TRUE))
+  
+  # read the shapefile
+  shpfile <- grep('\\.shp$', shpfiles, value=TRUE)
+  layer <- sf::st_layers(shpfile)
+  sfdat <- sf::st_read(shpfile, layer$name[[1]])
+  spdat <- as(shpdat, 'Spatial')
+  
+  # compute the centroids
+  centroids <- rgeos::gCentroid(spdat, byid=TRUE, id=spdat@data$GEOID)
+  
+  # add IDs to the centroids data.frame
+  centroidnames <- dimnames(centroids@coords)[[1]]
+  spdatnames <- as.character(spdat@data$GEOID)
+  stopifnot(all(centroidnames == spdatnames)) # just confirming that the merge will be correct. is there a better way to do the merge?
+  centroid_spdf <- SpatialPointsDataFrame(centroids, spdat@data, match.ID=FALSE)
+  
+  # filter out Northern Mariana Island (STATEFP=='69'), Guam (STATEFP=='66'), and American Samoa (STATEFP=='60')
+  centroid_spdf2 <- centroid_spdf[!(centroid_spdf$STATEFP %in% c('69','66','60')),]
+  # limit to the necessary columns
+  centroid_spdf3 <- centroid_spdf2[, c('GEOID')]
+  
+  # write to file
+  geojsonio::topojson_write(centroid_spdf3, file=viz$location)
+}
