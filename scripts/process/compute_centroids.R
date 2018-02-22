3c64cbc63f4ef792ee3df973654df843f1f84fa0
Alison Appling <aappling@usgs.gov>
2018-02-21 17:38:29 -0700

    temporarily limit centroids to just Arizona to get proj working

--- scripts/process/compute_centroids.R
+++ scripts/process/compute_centroids.R
@@ -11,7 +11,7 @@ process.compute_centroids <- function(viz) {
   shpfile <- grep('\\.shp$', shpfiles, value=TRUE)
   layer <- sf::st_layers(shpfile)
   sfdat <- sf::st_read(shpfile, layer$name[[1]])
-  spdat <- as(shpdat, 'Spatial')
+  spdat <- as(sfdat, 'Spatial')
   
   # compute the centroids
   centroids <- rgeos::gCentroid(spdat, byid=TRUE, id=spdat@data$GEOID)
@@ -20,8 +20,11 @@ process.compute_centroids <- function(viz) {
   centroidnames <- dimnames(centroids@coords)[[1]]
   spdatnames <- as.character(spdat@data$GEOID)
   stopifnot(all(centroidnames == spdatnames)) # just confirming that the merge will be correct. is there a better way to do the merge?
-  centroid_spdf <- SpatialPointsDataFrame(centroids, spdat@data, match.ID=FALSE)
+  centroid_spdf <- sp::SpatialPointsDataFrame(centroids, spdat@data, match.ID=FALSE)
   
+  # limit even more because centroids are a problem for our d3 projection?
+  centroid_spdf <- centroid_spdf[centroid_spdf$STATEFP %in% '04',]
+
   # filter out Northern Mariana Island (STATEFP=='69'), Guam (STATEFP=='66'), and American Samoa (STATEFP=='60')
   centroid_spdf2 <- centroid_spdf[!(centroid_spdf$STATEFP %in% c('69','66','60')),]
   # limit to the necessary columns
