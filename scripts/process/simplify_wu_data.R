c6355254d6e355052ba8c77b33834ff4a734752c
Laura DeCicco <ldecicco@usgs.gov>
2018-02-28 11:46:00 -0600

    better?

--- scripts/process/simplify_wu_data.R
+++ scripts/process/simplify_wu_data.R
@@ -7,10 +7,24 @@ process.simplify_wu_data <- function(viz) {
   orig_wu_type_col <- deps[["col_names"]][["orig_names"]]
   new_wu_type_col <- deps[["col_names"]][["new_names"]]
   
-  columns_to_keep <- c("STATE", "STATEFIPS", "COUNTY", "COUNTYFIPS", "FIPS", "YEAR")
+  columns_to_keep <- c("STATE", "STATEFIPS", "COUNTY", "COUNTYFIPS", "FIPS", "YEAR") # also double as character cols
   columns_to_rename <- c("TP-TotPop", orig_wu_type_col)
   new_names <- c("countypop", new_wu_type_col)
   
+  # none of the columns we kept had characters (-- or N/A) that caused the entire column to
+  # be read in as character and needed to be converted back to numeric, but in case
+  # we add more data as we go along, here is the code that does that:
+  which_numeric_cols <- !names(wu_df) %in% columns_to_keep
+  numeric_data <- wu_df[, which_numeric_cols]
+  fixed_numeric_data <- as.data.frame(sapply(numeric_data, function(v) { 
+    has_missing_symbol <- grep("--|N/A", v)
+    v[has_missing_symbol] <- NA
+    v <- as.numeric(v)
+    return(v)
+  }))
+  wu_df[, which_numeric_cols] <- fixed_numeric_data
+  
+  # now select only the pertinent columns
   wu_df_sel <- wu_df[, c(columns_to_keep, columns_to_rename)]
   names(wu_df_sel) <- c(columns_to_keep, new_names)
   
