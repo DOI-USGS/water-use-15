3b87745ef74d6cb2753fa4f9863a5e4b3c61dda8
Alison Appling <aappling@usgs.gov>
2018-03-12 21:06:15 -0700

    preprocess states with dict-based properties

--- scripts/preprocess/process_counties_to_states.js
+++ scripts/preprocess/process_counties_to_states.js
@@ -11,25 +11,31 @@ var topojson = require('topojson');
 var geojson = require('geojson');
 const argv = require('yargs').argv;
 
-// read in the topojson file with county boundaries
+// read in the topojson file with county boundaries and the json dictionary of state names and IDs
 var countyTopo = JSON.parse(fs.readFileSync(argv.counties, 'utf8'));
+var stateDict = JSON.parse(fs.readFileSync(argv.statedict, 'utf8'));
 
-function mergeCounties(countyTopo) {
+function mergeCounties(countyTopo, stateDict) {
   // get an array of state FIPS codes
-  var stateFipsObj = {};
-  countyTopo.objects.counties.geometries.forEach(function(county) {
-    stateFipsObj[county.properties.STATEFP] = 1;
-  })
-  var stateFips = Object.keys(stateFipsObj);
+  var stateFips = [];
+  stateDict.forEach(function(d) {
+    stateFips.push(d.STATE_FIPS);
+  });
   
   // for each state FIPS, find and merge all relevant counties
   var states = [];
   stateFips.forEach(function(fip) {
-      var state = topojson.merge(countyTopo, countyTopo.objects.counties.geometries.filter(function(d) {
-        return d.properties.STATEFP === fip;
-      }));
-      state['STATEFP'] = fip;
-      states.push(state);
+    var counties = countyTopo.objects.counties.geometries.filter(function(d) {
+      return d.properties.STATE_FIPS === fip;
+    });
+    var state = topojson.merge(countyTopo, counties);
+    var stateInfo = stateDict.filter(function(d) {
+      return d.STATE_FIPS === fip;
+    })[0];
+    state.STATE_FIPS = stateInfo.STATE_FIPS;
+    state.STATE_NAME = stateInfo.STATE_NAME;
+    state.STATE_ABBV = stateInfo.STATE_ABBV;
+    states.push(state);
   });
   
   /* format the states array into a geojson object. at this point one option would be to
@@ -39,11 +45,11 @@ function mergeCounties(countyTopo) {
   counties later. going with the second option for now. */
   statesGeojson = geojson.parse(
     states,
-    { MultiPolygon: 'coordinates', include: ['STATEFP'] },
+    { MultiPolygon: 'coordinates', include: ['STATE_FIPS','STATE_NAME','STATE_ABBV'] },
     crs = { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } });
   return statesGeojson;
 }
-statesGeojson = mergeCounties(countyTopo);
+statesGeojson = mergeCounties(countyTopo, stateDict);
 
 // write the output to topojson file
 statesTopojson = topojson.topology({ states: statesGeojson }, 1e8);
