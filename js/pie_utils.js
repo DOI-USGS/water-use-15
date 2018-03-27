e7eb5330d90d54994a5b9292d8425783d0deef26
Alison Appling <aappling@usgs.gov>
2018-03-27 16:19:52 -0700

    go back to county-specific radii

--- js/pie_utils.js
+++ js/pie_utils.js
@@ -4,7 +4,6 @@ var pie = d3.pie()
     .sort(null)
     .value(function(d) { return d.value; });
 
-var fixedRadius = 10;
 var arcpath = d3.arc()
     .innerRadius(0);
 
@@ -17,17 +16,16 @@ function addPieCharts() {
     .enter()
     .append('g')
       .classed("pie", true)
-      /*.attr("data-totalwu", function (d) {
+      .attr("data-totalwu", function (d) {
         //include as attribute to use for outerRadius for each arc
         //when arcs are added data is already subsetted to just the single category
         //but they need to use "total" to set the outer radius
         return d.piechartmeta[1]; 
-      })*/
+      })
       .attr("transform", function(d) {
         var xcoord = projectX(d.piechartmeta[0]),
             ycoord = projectY(d.piechartmeta[0]);
-        var radius = scaleCircles(d.piechartmeta[1]);
-        return "translate("+xcoord+","+ycoord+")scale("+(radius/fixedRadius)+")";
+        return "translate("+xcoord+","+ycoord+")";
       });
     
   
@@ -41,9 +39,6 @@ function addPieCharts() {
       .append("path")
         .classed("pieslice", true);
   
-  //var pieupdate = pieenter
-  //      .merge(pieslices); // merges update selection (where pieslices var is created) w/ enter selection (comes out of above line)
-  
   piesBaked = true;
   updatePieCharts();
   
@@ -51,20 +46,16 @@ function addPieCharts() {
 
 function updatePieCharts() {
   
-  d3.selectAll(".pieslice")
-    //.transition().duration(0)
-    .attr("d", arcpath.outerRadius(fixedRadius));
-  
   map.selectAll('.pie').selectAll('.pieslice')
-        //.transition().duration(0)
-        /*.attr("d", arcpath.outerRadius(function(d) {
-          var totalwuvalue = +d3.select(this.parentNode).attr("data-totalwu");
-          return scaleCircles(totalwuvalue);
-        }))*/
-        .attr("d", arcpath)
-        .attr("fill", function(d) { 
-          return categoryToColor(d.data.category); 
-        });
+    //.transition().duration(0)
+    .attr("d", arcpath.outerRadius(function(d) {
+      var totalwuvalue = +d3.select(this.parentNode).attr("data-totalwu");
+      return scaleCircles(totalwuvalue);
+    }))
+    .attr("d", arcpath)
+    .attr("fill", function(d) { 
+      return categoryToColor(d.data.category); 
+    });
   
 }
 
