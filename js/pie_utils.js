9bee09346ce5e50e4c5039438885abaf81a82e62
Alison Appling <aappling@usgs.gov>
2018-03-27 15:40:08 -0700

    try fixed pie radius w county-specific scale()

--- js/pie_utils.js
+++ js/pie_utils.js
@@ -4,9 +4,9 @@ var pie = d3.pie()
     .sort(null)
     .value(function(d) { return d.value; });
 
+var fixedRadius = 10;
 var arcpath = d3.arc()
     .innerRadius(0);
-    //.outerRadius(10);
 
 function addPieCharts() {
   
@@ -17,17 +17,17 @@ function addPieCharts() {
     .enter()
     .append('g')
       .classed("pie", true)
-      .attr("data-totalwu", function (d) {
+      /*.attr("data-totalwu", function (d) {
         //include as attribute to use for outerRadius for each arc
         //when arcs are added data is already subsetted to just the single category
         //but they need to use "total" to set the outer radius
         return d.piechartmeta[1]; 
-      })
+      })*/
       .attr("transform", function(d) {
         var xcoord = projectX(d.piechartmeta[0]),
             ycoord = projectY(d.piechartmeta[0]);
-        //var radius = scaleCircles(d.piechartmeta[1]);
-        return "translate("+xcoord+","+ycoord+")";// scale("+(radius/10)+")";
+        var radius = scaleCircles(d.piechartmeta[1]);
+        return "translate("+xcoord+","+ycoord+")scale("+(radius/fixedRadius)+")";
       });
     
   
@@ -51,13 +51,17 @@ function addPieCharts() {
 
 function updatePieCharts() {
   
+  d3.selectAll(".pieslice")
+    //.transition().duration(0)
+    .attr("d", arcpath.outerRadius(fixedRadius));
+  
   map.selectAll('.pie').selectAll('.pieslice')
         //.transition().duration(0)
-        .attr("d", arcpath.outerRadius(function(d) {
+        /*.attr("d", arcpath.outerRadius(function(d) {
           var totalwuvalue = +d3.select(this.parentNode).attr("data-totalwu");
           return scaleCircles(totalwuvalue);
-        }))
-        //.attr("d", arcpath)
+        }))*/
+        .attr("d", arcpath)
         .attr("fill", function(d) { 
           return categoryToColor(d.data.category); 
         });
