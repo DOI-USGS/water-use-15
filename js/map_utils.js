9f0c0f5b9168a046aaf4d306c6baef07cf83e4fd
Alison Appling <aappling@usgs.gov>
2018-03-27 17:37:29 -0700

    fuller separation between add and update circles

--- js/map_utils.js
+++ js/map_utils.js
@@ -35,7 +35,9 @@ function addCircles() {
   
   // uses globals map, countyCentroids, scaleCircles, activeCategory
   
-  map.selectAll('county-point')
+  var circleGroup = map.append('g')
+    .classed('circles', true);
+  circleGroup.selectAll('county-point')
     .data(countyCentroids.features)
     .enter()
     .append('circle')
@@ -43,24 +45,19 @@ function addCircles() {
     .sort(function(a,b) { 
       return d3.descending(a.properties[[activeCategory]], b.properties[[activeCategory]]);
     })
-    .attr('fips', function(d) { return d.properties.GEOID; })
-    .text(function(d) { return d.properties.GEOID; })
     .attr("cx", function(d) {
       var coordx = projectX(d.geometry.coordinates);
       if(coordx === 0) { console.log(d); } // moved outside of project function bc coordinates aren't always d.geometry.coordinates (like in pies)
       return coordx;
     })
     .attr("cy", function(d) { 
-      return projectY(d.geometry.coordinates)
-    })
-    .attr("r", function(d) { 
-      return scaleCircles(d.properties[[activeCategory]]);
+      return projectY(d.geometry.coordinates);
     })
     // this is OK to not worry about it changing on hover (activeCategory only changes on click) 
     // because people won't be able to see tooltips at the same time anyways
     .on("mouseover", function(d) { showToolTip(this, d, activeCategory); })
-    .on("mouseout", function(d) { hideTooltip(this, d); })
-    .style("fill", categoryToColor(activeCategory));
+    .on("mouseout", function(d) { hideTooltip(this, d); });
+    
   circlesAdded = true;
   
   // these newly added circles won't work until updateCircles is called, but
@@ -75,12 +72,14 @@ function updateCircles(category) {
   }
 
   d3.selectAll(".county-point")
-      .sort(function(a,b) { 
-        return d3.descending(a.properties[[category]], b.properties[[category]]);
-      })
-      //.transition().duration(0)
-      .attr("r", function(d) { return scaleCircles(d.properties[[category]]); })
-      .style("fill", categoryToColor(category));
+    .sort(function(a,b) { 
+      return d3.descending(a.properties[[category]], b.properties[[category]]);
+    })
+    //.transition().duration(0)
+    .attr("r", function(d) {
+      return scaleCircles(d.properties[[category]]);
+    })
+    .style("fill", categoryToColor(category));
       
   d3.selectAll(".legend-point")
     .transition().duration(600)
@@ -263,11 +262,7 @@ function updateCategory(category, prevCategory) {
       .attr("r", 0);
   
     // create or expand and update the pies
-    if (!piesBaked) {
-      addPieCharts();
-    } else {
-      updatePieCharts();
-    }
+    updatePieCharts();
     
   } else if(prevCategory === "piechart") {
   
@@ -276,7 +271,7 @@ function updateCategory(category, prevCategory) {
       //.transition().duration(0)
       .attr("d", arcpath.outerRadius(0));
       
-    // expand and update circles
+    // create or expand and update the circles
     updateCircles(category);
     
   } else {
