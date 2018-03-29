2432a0a0d7d01e87b1c48e7c04fc670685820953
Alison Appling <aappling@usgs.gov>
2018-03-28 21:03:47 -0700

    debugging - closer

--- js/pie_utils.js
+++ js/pie_utils.js
@@ -1,5 +1,9 @@
 // functions related to the pie charts
 
+var arcpath = d3.arc()
+  .innerRadius(0)
+  .outerRadius(1);
+
 function addPieSlices() {
   
   //relies on map as a global variable
@@ -7,24 +11,21 @@ function addPieSlices() {
   // add pie slices to each pie group
   map.selectAll('g.pie').selectAll('.pieslice')  
     .data(function(d) {
-      return [d.sliceGeomData];
+      return d.sliceGeomData;
     })
     .enter()
     .append("path")
       .classed("pieslice", true)
       .attr("fill", function(d) { 
         return categoryToColor(d.data.category); 
-      });
+      })
+      .attr("d", arcpath);
   
   piesBaked = true;
   
   // these newly added pie charts won't work until updatePieCharts is called, but
   // since this function always gets called from updatePieCharts, that should be fine
 }
-
-var arcpath = d3.arc()
-  .innerRadius(0)
-  .outerRadius(1);
 
 function updatePieSlices(prevTransition) {
   
