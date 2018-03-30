29adc58c277ba34f9a4e35e3a30d57e9221d605b
Jordan S Read <jread@usgs.gov>
2018-03-30 12:59:55 -0500

    updates for nested groups for transitions

--- js/circle_utils.js
+++ js/circle_utils.js
@@ -2,7 +2,7 @@ function addPieTins() {
   
   // uses globals map, countyCentroids, scaleCircles, activeCategory
   
-  map.selectAll('g.pie').selectAll('.tin')
+  map.selectAll('g.pie-scales').selectAll('.tin')
     .data(function(d) {
       return [d.properties];
     })
@@ -20,7 +20,7 @@ function addPieTins() {
   // since this function always gets called from updatePieTins, that should be fine
 }
 
-function updatePieTins(category, delay, duration) {
+function updatePieTins(category, delay) {
   
   // add the circles if needed
   if (!tinsAdded) {
@@ -29,13 +29,13 @@ function updatePieTins(category, delay, duration) {
 
   if(category === 'piechart') {
     d3.selectAll(".tin")
-      .transition().delay(delay).duration(duration)
+      .transition().delay(delay).duration(0)
       .style("fill", "transparent")
       .style("stroke", "lightgrey")
       .style("stroke-width", "0.01");
   } else {
     d3.selectAll(".tin")
-      .transition().delay(delay).duration(duration)
+      .transition().delay(delay).duration(0)
       .style("fill", categoryToColor(category))
       .style("stroke", "transparent"); // we should repoint this to use a lookup in styles.js
 
