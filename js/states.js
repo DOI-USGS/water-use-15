1b3eb9a0e27577e712eeca58e4afed7bef953f37
Alison Appling <aappling@usgs.gov>
2018-04-04 11:58:41 -0700

    add states.js

---
+++ js/states.js
@@ -0,0 +1,32 @@
+function updateStates(newView) {
+  console.log('updating states to new view: '+newView);
+  updateStateData(newView, updateStateBounds);
+}
+
+function updateStateData(newView, callback) {
+  if(newView === 'USA') {
+    callback(null, stateBoundsUSA);
+  } else {
+    if(!stateBoundsZoom) {
+      d3.json('data/state_boundaries_zoom.json', function(error, stateBoundsTopo) {
+        // cache the data to a global variable
+        stateBoundsZoom = topojson.feature(stateBoundsTopo, stateBoundsTopo.objects.states);
+        // do the update
+        callback(null, stateBoundsZoom);
+      });
+    } else {
+      // do the update
+      callback(null, stateBoundsZoom);
+    }
+  }
+}
+
+function updateStateBounds(error, stateBounds) {
+  console.log('updating state bounds');
+  map.select('#state-bounds')
+    .selectAll( 'path.state' )
+    .data(stateBounds.features, function(d) {
+      return d.properties.STATE_ABBV;
+    })
+    .attr('d', buildPath);  
+}
