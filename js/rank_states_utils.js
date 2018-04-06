40f5b4368e0cb74d05004c4c6154b23f7a9f8747
Jordan S Read <jread@usgs.gov>
2018-04-06 10:44:33 -0500

    simple svg

--- js/rank_states_utils.js
+++ js/rank_states_utils.js
@@ -3,8 +3,12 @@ var svgStates = d3.select("#rank-states-interactive")
   .append("svg")
   .attr('viewBox', '0 0 1000 300')
   .attr('preserveAspectRatio', 'xMidYMid');
-svgStates.append("g")
-  .append('rect')
+var boxes = svgStates.append("g");
+boxes.append('rect')
     .attr('fill','red')
     .attr('height',"300")
     .attr('width',"1000");
+boxes.append('circle')
+    .attr('fill', 'white')
+    .attr('r','2')
+    .attr('transform','translate(200,100)scale(20)');
