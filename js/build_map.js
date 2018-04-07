d8f7017727bc7fff34476d95697f87cfec534130
Alison Appling <aappling@usgs.gov>
2018-04-06 17:13:51 -0700

    organize build_map more

--- js/build_map.js
+++ js/build_map.js
@@ -18,7 +18,7 @@ var buildPath = d3.geoPath()
 var scaleCircles = d3.scaleSqrt()
   .range([0, 15]);
     
-//Create container
+// Create container
 var container = d3.select('body')
   .append('div')
   .classed('svg-container', true);
@@ -27,7 +27,7 @@ var container = d3.select('body')
 var tooltipDiv = d3.select("body").append("div")
   .classed("tooltip hidden", true);
 
-// Create SVG
+// Create SVG and map
 var svg = d3.select(".svg-container")
   .append("svg")
   .attr('viewBox', '0 0 ' + chart_width + ' ' + chart_height + '')
@@ -42,27 +42,29 @@ var mapBackground = map.append("rect")
   .attr("height", chart_height)
   .on('click', zoomToFromState);
 
-// Datasets
+// Globals
+var activeView, activeCategory, prevCategory;
 var stateBoundsUSA, stateBoundsZoom, countyBoundsUSA, countyCentroids, circlesPaths;
 var countyBoundsZoom = new Map();
 var circlesAdded = false;
 
+// Set up some map info and map elements so we're ready to add data piece by piece
+readHashes();
+prepareMap();
+
+// Read data and add to map
 d3.queue()
   .defer(d3.json, "data/state_boundaries_USA.json")
   .defer(d3.json, "data/county_centroids_wu.json")
   .defer(d3.json, "data/wu_data_15_range.json")
   .await(fillMap);
 
-// Zoom status: default is nation-wide
-var activeView = getHash('view');
-if(!activeView) activeView = 'USA';
+// Add buttons  
+addButtons();
 
-// Water use category: default is total. To make these readable in the URLs,
-// let's use full-length space-removed lower-case labels, e.g. publicsupply and thermoelectric
-var activeCategory = getHash('category');
-if(!activeCategory) activeCategory = 'total';
-// default for prev is total
-var prevCategory = 'total';
+
+/** Functions **/
+
 function renameCountyData(data) {
   // reverse the column name simplifications done in merge_centroids_wu.R
   dataVerbose = [];
@@ -85,27 +87,45 @@ function renameCountyData(data) {
   return(dataVerbose);
 }
 
-svg.append("text")
-  .attr("id", "maptitle")
-  .attr("x", chart_width/2)
-  .attr("y", chart_height*0.10); // bring in 10% of chart height
+function readHashes() {
+  // Zoom status: default is nation-wide
+  activeView = getHash('view');
+  if(!activeView) activeView = 'USA';
+  
+  // Water use category: default is total. To make these readable in the URLs,
+  // let's use full-length space-removed lower-case labels, e.g. publicsupply and thermoelectric
+  activeCategory = getHash('category');
+  if(!activeCategory) activeCategory = 'total';
+  // default for prev is total
+  prevCategory = 'total';
+}
 
 function prepareMap() {
+
+  /** Add map elements **/
+  
+  // add placeholder groups for geographic boundaries and circles
+  map.append('g').attr('id', 'county-bounds');
+  map.append('g').attr('id', 'state-bounds');
+  map.append('g').attr('id', 'wu-circles');
+  
+  // add watermark
+  addWatermark();
+
+  // create map title
+  svg.append("text")
+    .attr("id", "maptitle")
+    .attr("x", chart_width/2)
+    .attr("y", chart_height*0.10); // bring in 10% of chart height
+  
+  /** Initialize title and URL **/
+  
   // Initialize page info
   updateTitle(activeCategory);
   setHash('view', activeView);
   setHash('category', activeCategory);
   
-  // add watermark
-  addWatermark();
 }
-prepareMap();
-
-// add placeholder groups for geographic boundaries and circles
-map.append('g').attr('id', 'county-bounds');
-map.append('g').attr('id', 'state-bounds');
-map.append('g').attr('id', 'wu-circles');
-
 
 function fillMap() {
 
@@ -140,33 +160,35 @@ function fillMap() {
   updateCounties('USA');
 }
 
-
-var buttonContainer = d3.select('.svg-container')
-  .append('div')
-  .attr('id', 'button-container');
-  
-buttonContainer.append('div').classed('select-arrowbox', true);
-  
-var categoryButtons = d3.select('#button-container')
-  .selectAll('button')
-  .data(categories)
-  .enter()
-  .append('button')
-  .text(function(d){
-    return categoryToName(d);
-  })
-  .attr('class', function(d){
-    return d;
-  })
-  .on('click', function(d){
-    prevCategory = activeCategory;
-    activeCategory = d.toLowerCase(); // put this here so it only changes on click
-    updateCategory(activeCategory, action = 'click');
-  })
-  .on('mouseover', function(d){
-    updateCategory(d.toLowerCase(), activeCategory, action = 'mouseover');
-  })
-  .on('mouseout', function(d){
-    updateCategory(activeCategory, d.toLowerCase(),
-    action = 'mouseout');
-  });
+function addButtons() {
+  var buttonContainer = d3.select('.svg-container')
+    .append('div')
+    .attr('id', 'button-container');
+    
+  buttonContainer.append('div')
+    .classed('select-arrowbox', true);
+    
+  d3.select('#button-container')
+    .selectAll('button')
+    .data(categories)
+    .enter()
+    .append('button')
+    .text(function(d){
+      return categoryToName(d);
+    })
+    .attr('class', function(d){
+      return d;
+    })
+    .on('click', function(d){
+      prevCategory = activeCategory;
+      activeCategory = d.toLowerCase(); // put this here so it only changes on click
+      updateCategory(activeCategory, action = 'click');
+    })
+    .on('mouseover', function(d){
+      updateCategory(d.toLowerCase(), activeCategory, action = 'mouseover');
+    })
+    .on('mouseout', function(d){
+      updateCategory(activeCategory, d.toLowerCase(),
+      action = 'mouseout');
+    });
+}
