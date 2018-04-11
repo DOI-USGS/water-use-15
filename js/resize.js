118bcf2f175b451f48e1d6496747eab2f9143d46
Alison Appling <aappling@usgs.gov>
2018-04-11 14:59:31 -0700

    various resizing adjustments

--- js/resize.js
+++ js/resize.js
@@ -12,9 +12,10 @@ function resize() {
   if(waterUseViz.mode === 'desktop') {
   
     // buttonBox is at the left and centered vertically
-    waterUseViz.dims.buttonBox.y0 = (waterUseViz.dims.map.height/2) - (waterUseViz.dims.buttonBox.height/2);
     waterUseViz.dims.buttonBox.width = waterUseViz.dims.buttonBox.widthDesktop;
     waterUseViz.dims.buttonBox.height = waterUseViz.dims.buttonBox.heightDesktop;
+    waterUseViz.dims.buttonBox.x0 = 0;
+    waterUseViz.dims.buttonBox.y0 = (waterUseViz.dims.map.height/2) - (waterUseViz.dims.buttonBox.height/2);
     // map fills the full svg
     waterUseViz.dims.map.x0 = waterUseViz.dims.buttonBox.width;
     // svg is [buttons][map]
@@ -24,22 +25,23 @@ function resize() {
     waterUseViz.dims.watermark.x0 = waterUseViz.dims.svg.width * 0.01;
     waterUseViz.dims.watermark.y0 = waterUseViz.dims.svg.height * 0.95;
     
-  } else {
+  } else if(waterUseViz.mode === 'mobile') {
   
     // buttonBox sits below map with small vertical buffer between map and buttons
-    waterUseViz.dims.buttonBox.y0 = waterUseViz.dims.map.height * 1.05;
-    waterUseViz.dims.buttonBox.width = waterUseViz.dims.map.width;
-    waterUseViz.dims.buttonBox.height = waterUseViz.dims.buttonBox.width *
+    waterUseViz.dims.buttonBox.width = waterUseViz.dims.map.width * 0.6;
+    waterUseViz.dims.buttonBox.height = waterUseViz.dims.buttonBox.width * 0.5 *
       (waterUseViz.dims.buttonBox.heightDesktop / waterUseViz.dims.buttonBox.widthDesktop);
+    waterUseViz.dims.buttonBox.x0 = (waterUseViz.dims.svg.width - waterUseViz.dims.buttonBox.width) / 2; // center within svg
+    waterUseViz.dims.buttonBox.y0 = waterUseViz.dims.map.height * 1.02;
     // map fills the top part of the svg
     waterUseViz.dims.map.x0 = 0;
     // svg is [map]
     //        [buttons]
     waterUseViz.dims.svg.width = waterUseViz.dims.map.width;
     waterUseViz.dims.svg.height = waterUseViz.dims.buttonBox.y0 + waterUseViz.dims.buttonBox.height;
-    // watermark is at bottom right
+    // watermark is at top right
     waterUseViz.dims.watermark.x0 = waterUseViz.dims.svg.width * 0.85;
-    waterUseViz.dims.watermark.y0 = waterUseViz.dims.svg.height * 0.95;
+    waterUseViz.dims.watermark.y0 = waterUseViz.dims.svg.height * 0.05;
   }
   
   // Apply the changes to the svg, map, map background, and watermark
@@ -51,7 +53,7 @@ function resize() {
     .attr("width", waterUseViz.dims.svg.width)
     .attr("height", waterUseViz.dims.map.height);
   watermark
-    .attr('transform', 'translate(' + waterUseViz.dims.watermark.x0 + ',' + waterUseViz.dims.watermark.x0 + ')scale(0.25)');
+    .attr('transform', 'translate(' + waterUseViz.dims.watermark.x0 + ',' + waterUseViz.dims.watermark.y0 + ')scale(0.25)');
   
   // Apply the changes to the button elements
   resizeButtons();
