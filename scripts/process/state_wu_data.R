77336fdaa97438776ed5ecbbca64d639b32522bb
Laura DeCicco <ldecicco@usgs.gov>
2018-04-17 14:11:26 -0500

    Adding state data in process

---
+++ scripts/process/state_wu_data.R
@@ -0,0 +1,18 @@
+#' Summarize water use data to national totals
+#'
+process.state_wu_data <- function(viz) {
+  
+  deps <- readDepends(viz)
+  wu_df <- deps[["rawdata"]]
+
+  # calculate national sums of each category
+  wu_df_state <- dplyr::select(wu_df, STATE, total) %>%
+    dplyr::group_by(STATE) %>%
+    dplyr::summarise(wu = round(sum(total))) %>%
+    dplyr::mutate(open = dplyr::case_when(STATE %in% viz[["drag_states"]] ~ TRUE, TRUE ~ FALSE))%>%
+    dplyr::rename(abrv=STATE)
+  
+  # calculate other category value
+  
+  jsonlite::write_json(wu_df_state, viz[["location"]] )
+}
