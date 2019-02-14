13df3cef3bdd7e0cedef938471c680a727ceb4f0
wdwatkins <wwatkins@usgs.gov>
2019-02-13 16:34:11 -0800

    not using build script

--- docker_build_viz.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-viz_folder_name=water-use-15
-docker start ${viz_folder_name}
-docker cp ./. ${viz_folder_name}:/home/rstudio/${viz_folder_name}
-docker exec ${viz_folder_name} Rscript -e 'vizlab::vizmake()'
-docker cp ${viz_folder_name}:/home/rstudio/${viz_folder_name}/. .
-docker stop ${viz_folder_name}
